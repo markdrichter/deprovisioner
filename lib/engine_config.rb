caf59d7e2c5739825a78da48306d09c354acb0b6
Mark Richter <mrichter@thoughtworks.com>
2020-02-15 15:48:40 -0800

    Update Copyright

--- lib/engine_config.rb
+++ lib/engine_config.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 Mark D. Richter
+# Copyright 2018,2019,2020 Mark D. Richter
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
