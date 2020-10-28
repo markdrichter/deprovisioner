845db341ec1d99944de896c13e3ca9cbc3e8cb10
Mark Richter <mark@markr.com>
2020-10-28 10:26:10 -0700

    source gam locally

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 # export CONFIG_YML=<your config.yml>
-# sh build.sh <gcp-project> <gcp-bucket> <environment> <registry>
+# sh build.sh <gcp-project> <gcp-bucket> <registry>
 
 export GOOGLE_CLOUD_PROJECT=$1
 export BUCKET=$2
 export REGISTRY=$3
 export CONFIG_YML=config.yml
 gcloud config set project $GOOGLE_CLOUD_PROJECT
-gsutil -m cp -r gs://${BUCKET}/gam .
-chmod +x gam/gam
 export THETAG=${REGISTRY}/${GOOGLE_CLOUD_PROJECT}/deprovisioner:latest
 docker build --build-arg CONFIG=$CONFIG_YML -f docker/Dockerfile --tag ${THETAG} .
 docker push ${REGISTRY}/${GOOGLE_CLOUD_PROJECT}/deprovisioner:latest
